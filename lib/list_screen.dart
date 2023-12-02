61eb61d0c9f8ba2bff6c6d3552acd490095438fb
abhishek shetty <abhisheksheety234@gmail.com>
2023-12-02 14:15:53 +0530

    this code is commit now

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:salon_page/screens/popup.dart';
 import 'package:salon_page/screens/service_screen.dart';
 
 class ListScreen extends StatefulWidget {
@@ -9,6 +8,7 @@ class ListScreen extends StatefulWidget {
 
 class _ListScreenState extends State<ListScreen> {
   List<Services> selectedServices = [];
+  double totalAmount = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -53,14 +53,15 @@ class _ListScreenState extends State<ListScreen> {
                         _toggleService(service);
                         _showSelectedServices(context);
                       },
-                      child: Text(selectedServices.contains(service) ? 'Remove' : 'Add'),
+                      child: Text(selectedServices.contains(service)
+                          ? 'Remove'
+                          : 'Add',style: TextStyle(color: Colors.black),),
                     ),
                   ),
                 );
               },
             ),
           ),
-
         ],
       ),
     );
@@ -70,16 +71,30 @@ class _ListScreenState extends State<ListScreen> {
     setState(() {
       if (selectedServices.contains(service)) {
         selectedServices.remove(service);
+        totalAmount -=
+            double.tryParse(service.price.replaceAll(RegExp(r'[^\d.]'), '')) ??
+                0.0;
       } else {
         selectedServices.add(service);
+        totalAmount +=
+            double.tryParse(service.price.replaceAll(RegExp(r'[^\d.]'), '')) ??
+                0.0;
       }
     });
   }
 
   void _showSelectedServices(BuildContext context) {
-    showModalBottomSheet(
+    double totalAmount = 0.0;
+
+    showModalBottomSheet(backgroundColor:Colors.grey,
       context: context,
       builder: (BuildContext context) {
+        for (Services service in selectedServices) {
+          String priceString = service.price.replaceAll(RegExp(r'[^\d.]'), '');
+          double price = double.tryParse(priceString) ?? 0.0;
+          totalAmount += price;
+        }
+
         return Container(
           padding: EdgeInsets.all(16.0),
           child: Column(
@@ -89,30 +104,35 @@ class _ListScreenState extends State<ListScreen> {
               for (Services service in selectedServices) ...[
                 SizedBox(height: 8, width: 40),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       service.name,
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
+                    SizedBox(width: 10,),
                     Text(
                       service.price,
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
+
               ],
               SizedBox(height: 16, width: 40),
+              Text(
+                'Total Amount: Rs. $totalAmount',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
               ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                child: Text('Book Appointment'),
+                child:Column(children: [
+        Text('Book Appointment'),
+        ]
+                )
               ),
             ],
           ),
--- lib/screens/service_screen.dart
+++ lib/screens/service_screen.dart
@@ -16,11 +16,11 @@ class Services {
 }
 
   List<Services>ServicesList=[
-    Services(name: 'Mens Haircut', price: 'Rs.200/-', description: '(professional haircut that suits your face shape and specially trained stylist for Men.)', images:  'image/haircut.jpeg',),
-Services(name:'Mens shaving', price: 'Rs.200/-', description: '(professional beard grooming that suits your face shape & Smooth and Straight for a fresh look.)', images:  'image/shaving.jpeg',),
-Services(name: 'Hair colour',price: 'Rs.300/-', description: '(Even and mess-free color application.)', images: 'image/haircolor.jpeg',),
-Services(name: 'Face care', price: 'Rs.500/-', description: '(Cleansing of neck, face along with scrubbing and deep cleaning of face to remove dead skin.)', images:  'image/facecare.jpeg',),
-Services(name:'Massage', price: 'Rs.400/-', description: '(Relaxing Oil massage to treat stiff / tense muscle & relieve stress.)', images:  'image/massage.jpeg',),
+    Services(name: 'Mens Haircut', price: 'Rs 200/-', description: '(professional haircut that suits your face shape and specially trained stylist for Men.)', images:  'image/haircut.jpeg',),
+Services(name:'Mens shaving', price: 'Rs 200/-', description: '(professional beard grooming that suits your face shape & Smooth and Straight for a fresh look.)', images:  'image/shaving.jpeg',),
+Services(name: 'Hair colour',price: 'Rs 300/-', description: '(Even and mess-free color application.)', images: 'image/haircolor.jpeg',),
+Services(name: 'Face care', price: 'Rs 500/-', description: '(Cleansing of neck, face along with scrubbing and deep cleaning of face to remove dead skin.)', images:  'image/facecare.jpeg',),
+Services(name:'Massage', price: 'Rs 400/-', description: '(Relaxing Oil massage to treat stiff / tense muscle & relieve stress.)', images:  'image/massage.jpeg',),
 
 
 
